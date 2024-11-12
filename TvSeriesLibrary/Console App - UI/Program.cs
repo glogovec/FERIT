44d2d2d8cd47c9c1ba3bd11b2c1c8dbd10ee2d0c
glogovec <lukahaban13@gmail.com>
2024-11-12 14:32:00 +0100

    Add files via upload
    Homework 1 - basics of OOP

---
+++ TvSeriesLibrary/Console App - UI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TvSeriesLibrary;
+
+namespace TvSeriesUi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Episode ep1 = new Episode();
+            Episode ep2 = new Episode(10, 64.39, 8.7);
+
+            int viewers = 10;
+
+            for (int i = 0; i < viewers; i++)
+            {
+                ep1.AddView(GenerateRandomScore());
+                Console.WriteLine(ep1.GetMaxScore());
+            }
+
+            if (ep1.GetAverageScore() > ep2.GetAverageScore())
+            {
+                Console.WriteLine($"Viewers: {ep1.GetViewerCount()}");
+            }
+
+            else
+            {
+                Console.WriteLine($"Viewers: {ep2.GetViewerCount()}");
+            }
+        }
+        public static double GenerateRandomScore()
+        {
+            Random random = new Random();   
+            return random.NextDouble() * 10; // NextDouble() generates doubles from 0.0 do 1.0 so we have to multiply it by 10 
+        }
+    }
+}
---
+++ TvSeriesLibrary/TvSeriesLibrary/Episode.cs
@@ -0,0 +1,34 @@
+﻿namespace TvSeriesLibrary
+{
+    public class Episode
+    {
+        private int viewersCount;
+        private double totalScore;
+        private double maxScore;
+
+        public Episode() : this(0, 0.0, 0.0) { } //nice tip from AV 2
+
+        public Episode(int viewersCount, double totalScore, double maxScore)
+        {
+            this.viewersCount = viewersCount;
+            this.totalScore = totalScore;
+            this.maxScore = maxScore;
+        }
+
+        public void AddView(double score)
+        {
+            viewersCount++;
+            totalScore += score;
+            if (score > maxScore)
+                maxScore = score;
+        }
+
+        public double GetMaxScore() { return maxScore; }
+
+        public double GetAverageScore() { return totalScore / viewersCount; }
+
+        public int GetViewerCount() { return viewersCount; }
+
+
+    }
+}
